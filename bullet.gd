d81bdd79de143c30b142e9730a717eeef1282668
cosmobrain0 <cosmobrain@outlook.com>
2024-08-26 10:17:17 +0100

    nice particles for the bullets

--- bullet.gd
+++ bullet.gd
@@ -6,6 +6,9 @@ var speed := 500.0
 var velocity: Vector2
 var facing := Facing.LEFT
 var destroyed := false
+var particles_dead := false
+@onready var particle_generator: CPUParticles2D = $CPUParticles2D
+@onready var particlesAfterDeathTimer = $ParticlesAfterDeathTimer
 @onready var sprite := $Sprite2D
 
 func direction() -> Vector2:
@@ -18,21 +21,35 @@ func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 	area_entered.connect(_on_area_entered)
 	velocity = speed * direction()
+	particle_generator.direction = -direction()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if destroyed:
-		queue_free()
+		if particles_dead: queue_free()
 		return
 	position += velocity * delta
 
 func _on_body_entered(body: Node2D):
+	destroy_self()
+
+func destroy_self():
 	destroyed = true
+	monitoring = false
+	monitorable = false
+	sprite.visible = false
+	particle_generator.emitting = false
+	particlesAfterDeathTimer.start()
+	particlesAfterDeathTimer.timeout.connect(_on_destruction_timer_timeout)
+
+func _on_destruction_timer_timeout():
+	particles_dead = true
 
 func _on_area_entered(area: Area2D):
 	if area.get_script().get_global_name() == "Goomba":
 		SignalBus.goomba_shot.emit(self, area)
+		destroy_self()
 
 func _on_bullet_spawn(bullet: Node2D, bullet_facing: Facing):
 	if bullet == self:
@@ -45,3 +62,4 @@ func set_facing(new_facing: Facing):
 		sprite.frame = 1
 	else:
 		sprite.frame = 0
+	particle_generator.direction = -direction()
