ad9f0c12f1cc539e2735e839b86a8354fb774e49
cosmobrain0 <cosmobrain@outlook.com>
2024-08-27 08:02:39 +0100

    camera scrolls to the right

--- camera_movement.gd
+++ camera_movement.gd
@@ -3,36 +3,38 @@ extends Node2D
 enum Facing {LEFT, RIGHT}
 
 var levels: Array[Resource] = [preload("res://levels/level-0.tscn")]
+@onready var previous_scene: TileMapLayer = null
+@onready var current_scene: TileMapLayer = $TileMapLayer
+@onready var next_scene: TileMapLayer
 @onready var player := $Player
 var camera_origin := Vector2.ZERO
+var movement_speed := 200.0
+var left_bound_for_player := -50.0
 
-const dead_zone := Vector2(100, 100*9/16)
-const centre_facing_right := Vector2(1920/2 - 500, 1080/2)
-const centre_facing_left := Vector2(1920/2 + 500, 1080/2)
-var centre := centre_facing_right
-const movement_coefficient := 1.5
+@onready var camera_origin_x_at_last_level_spawn := 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var level: TileMapLayer = levels[0].instantiate()
-	level.position = Vector2(1920, 0)
-	get_tree().root.add_child.call_deferred(level)
-	SignalBus.player_facing_changed.connect(_on_player_facing_changed)
-
-func _on_player_facing_changed(facing: Facing) -> void:
-	if facing == Facing.LEFT: centre = centre_facing_left
-	else: centre = centre_facing_right
+	next_scene = levels[0].instantiate()
+	next_scene.position = Vector2(1920, 0)
+	get_tree().root.add_child.call_deferred(next_scene)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var screen_coord: Vector2 = get_viewport().get_screen_transform() * get_global_transform_with_canvas() * player.position
-	var offset := screen_coord - centre
-	if abs(offset.x) >= dead_zone.x:
-		var distance: float = (abs(offset.x)-dead_zone.x) * movement_coefficient * delta
-		if offset.x < 0.0:
-			distance *= -1.0
-		set_origin(camera_origin - Vector2(distance, 0))
-	# TODO: add some y coordinate shenanigans maybe?
+	set_origin(camera_origin - Vector2(movement_speed * delta, 0))
+	var screen_player_pos: Vector2 = get_viewport().get_screen_transform() * get_global_transform_with_canvas() * player.position
+	if screen_player_pos.x <= left_bound_for_player:
+		get_tree().reload_current_scene()
+		return
+	if camera_origin_x_at_last_level_spawn + 1700 <= -camera_origin.x:
+		camera_origin_x_at_last_level_spawn += 1920
+		if previous_scene != null:
+			previous_scene.queue_free()
+		previous_scene = current_scene
+		current_scene = next_scene
+		next_scene = levels[0].instantiate()
+		next_scene.position = Vector2(camera_origin_x_at_last_level_spawn + 1920, 0)
+		get_tree().root.add_child.call_deferred(next_scene)
 
 func set_origin(new_origin: Vector2) -> void:
 	camera_origin = new_origin
