0c28c94234c332aa945e6caec8ace568cf5beefa
cosmobrain0 <cosmobrain@outlook.com>
2024-08-29 07:04:09 +0100

    score increase indicator is nice now

--- score.gd
+++ score.gd
@@ -7,7 +7,7 @@ var score_update_speed = 300.0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	SignalBus.score_changed.connect(_on_score_changed)
-
+	update_text()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -16,7 +16,7 @@ func _process(delta: float) -> void:
 	change *= minf(score_update_speed * delta, abs(change)) / abs(change)
 	set_current_score(current_score + change)
 
-func _on_score_changed(old_score: float, new_score: float) -> void:
+func _on_score_changed(old_score: float, new_score: float, position: Vector2) -> void:
 	target_score = new_score
 
 func set_current_score(new_score: float):
@@ -26,5 +26,10 @@ func set_current_score(new_score: float):
 func update_text():
 	if target_score != current_score:
 		var delta := target_score - current_score
-		text = "[b]Score[/b]: [color=green](%s+)[/color] %s\n[b]High Score[/b]: %s" % [str(floorf(delta)), str(floorf(current_score)), str(floorf(ScoreManager.high_score))]
+		var format: String
+		if delta == 0:
+			format = "[b]Score[/b]: %s\n[b]High Score[/b]: %s"
+		else:
+			format = "[b]Score[/b]: [color=green]%s[/color]\n[b]High Score[/b]: %s"
+		text = format % [str(floorf(current_score)), str(floorf(ScoreManager.high_score))]
 	else: text = "[b]Score[/b]: %s\n[b]High Score[/b]: %s" % [str(floorf(current_score)), str(floorf(ScoreManager.high_score))]
--- score_manager.gd
+++ score_manager.gd
@@ -2,6 +2,7 @@ extends Node
 
 var high_score := 0.0
 var current_score := 0.0
+var score_increase_scene = preload("res://score_increase_indicator.tscn")
 
 func _ready():
 	SignalBus.game_restart.connect(_on_game_restart)
@@ -11,13 +12,17 @@ func _on_game_restart() -> void:
 	high_score = maxf(current_score, high_score)
 	current_score = 0.0
 
-func _on_score_changed(old_score: float, new_score: float) -> void:
+func _on_score_changed(old_score: float, new_score: float, position: Vector2) -> void:
 	current_score = new_score
+	var scene: ScoreIncreaseIndicator = score_increase_scene.instantiate()
+	scene.position = position
+	scene.score_value = new_score - old_score
+	get_tree().root.add_child(scene)
 
-func set_score(new_score: float) -> void:
+func set_score(new_score: float, position: Vector2) -> void:
 	var old_score: float = current_score
 	if new_score != old_score:
-		SignalBus.score_changed.emit(old_score, new_score)
+		SignalBus.score_changed.emit(old_score, new_score, position)
 
-func change_score(change: float) -> void:
-	set_score(current_score + change)
+func change_score(change: float, position: Vector2) -> void:
+	set_score(current_score + change, position)
